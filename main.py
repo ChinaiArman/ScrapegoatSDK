2c6ebf5d239ddc3a9f71ce39c32af1ae1d346bbd
ChinaiArman <chinaiarman@gmail.com>
2025-11-22 02:24:42 -0800

    Add modular subclasses, enable sheepdog for extension

--- main.py
+++ main.py
@@ -5,28 +5,27 @@ from scrapegoat import Shepherd, Gardener, Sheepdog
 from scrapegoat_loom import Loom
 
 
+
+class NewSheepDog(Sheepdog):
+    """
+    """
+    def getter(self, url: str, **kwargs) -> str:
+        """
+        """
+        print(f"Fetching URL: {url}")
+        return super().getter(url, **kwargs)
+
 def main():
     """
     """
-    # shepherd = Shepherd()
+    # SHEPHERD EXAMPLE
+    shepherd = Shepherd(sheepdog=NewSheepDog())
+    shepherd.herd("example.goat")
 
-    # query = """
-    # VISIT "https://en.wikipedia.org/wiki/Web_scraping";
-    # SCRAPE p;
-    # EXTRACT id, body;
-    # OUTPUT csv --filename "test1" --filepath "./outputs";
-    # VISIT "https://en.wikipedia.org/wiki/Dog";
-    # SCRAPE p;
-    # EXTRACT id, body;
-    # OUTPUT csv --filename "test2" --filepath "./outputs";
-    # """
-
-    # shepherd.herd("example.goat")
-
-
-    html = Sheepdog().fetch("https://en.wikipedia.org/wiki/Web_scraping")
-    root = Gardener().grow_tree(html)
-    Loom(root).weave()
+    # LOOM EXAMPLE
+    # html = Sheepdog().fetch("https://en.wikipedia.org/wiki/Web_scraping")
+    # root = Gardener().grow_tree(html)
+    # Loom(root).weave()
 
 
 if __name__ == "__main__":
--- scrapegoat/src/scrapegoat/classes/sheepdog.py
+++ scrapegoat/src/scrapegoat/classes/sheepdog.py
@@ -1,12 +1,24 @@
 """
 """
 from typing import Union
+import requests
 
 from .command import FetchCommand
 
 class Sheepdog:
     """
     """
+    DEFAULT_HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Scrapegoat)",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Accept": "*/*",
+        "DNT": "1",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+    }
     def __init__(self):
         """
         """
@@ -17,8 +29,16 @@ class Sheepdog:
         """
         if not isinstance(fetch_command, FetchCommand):
             fetch_command = FetchCommand(fetch_command)
+        fetch_command.set_getter(self.getter)
         return fetch_command.execute()
     
+    def getter(self, url: str, **kwargs) -> str:
+        """
+        """
+        headers = kwargs.pop('headers', self.DEFAULT_HEADERS)
+        response = requests.get(url, headers=headers, **kwargs)
+        response.raise_for_status()
+        return response.text
 
 def main():
     """
